42543dd4e0a34afdbc85dd93e5bda7978ab55372
fallou-dev <serignefalloundiaye6@esp.sn>
2023-03-02 09:40:36 +0000

    promo

--- app/Http/Controllers/PromoController.php
+++ app/Http/Controllers/PromoController.php
@@ -7,15 +7,24 @@ use App\Http\Requests\PromoUpdateRequest;
 use App\Http\Resources\PromoCollection;
 use App\Http\Resources\PromoResource;
 use App\Models\Promo;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PromoController extends Controller
 {
     public function index()
     {
+        if ((auth()->user()->cannot('manage_promo') || auth()->user()->can('view_promo')) && (auth()->user()->can('manage_promo') || auth()->user()->cannot('view_promo'))){
+            return response([
+                "message" => "vous n'avez pas le droit",
+             ]);
+         
+          }
+          else{
         $promos = Promo::where('is_active','=','1')->get();
 
         return new PromoCollection($promos);
+          }
     }
 
 
@@ -26,6 +35,15 @@ class PromoController extends Controller
 
     public function store(PromoStoreRequest $request)
     {
+
+        if ($request->user()->cannot('manage_promo')){
+           return response([
+               "message" => "vous n'avez pas le droit",
+            ]);
+        
+         }
+
+         else{
         
         $user_id=auth()->user()->id;
         
@@ -38,22 +56,39 @@ class PromoController extends Controller
        
 
         return new PromoResource($promo);
+           }
     }
 
     public function update(PromoUpdateRequest $request, Promo $promo)
     {
+        if ($request->user()->cannot('manage_promo')){
+            return response([
+                "message" => "vous n'avez pas le droit",
+             ]);
+         
+          }
+          else{
         
         $promo->update($request->validated());
 
         return new PromoResource($promo);
+          }
     }
 
     public function destroy( Promo $promo)
     {
+        if ($request->user()->cannot('manage_promo')){
+            return response([
+                "message" => "vous n'avez pas le droit",
+             ]);
+         
+          }
+          else{
         $promo->update([
             'is_active' => 0
         ]);
 
         return response()->noContent();
+     }
     }
 }
