ec0a8abc9710b76af88234892de7c97b39275c16
mouhamed3008 <mouha712@gmail.com>
2023-04-18 10:26:52 +0000

    not utile

--- app/Models/Promo.php
+++ app/Models/Promo.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+
 use eloquentFilter\QueryFilter\ModelFilters\Filterable;
 
 class Promo extends Model
@@ -57,13 +56,13 @@ class Promo extends Model
     {
         return $this->hasMany(PromoReferentiel::class);
     }
-    
+
     public function referentiels()
     {
         return $this->belongsToMany(Referentiel::class, 'promo_referentiels')->wherePivot('is_active', 1);;
     }
-    
-  
-    
+
+
+
 }
 
