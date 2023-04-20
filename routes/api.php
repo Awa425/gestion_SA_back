dc6afb1b1ea4276c89400b5fbac1542d06da10b7
mouhamed3008 <mouha712@gmail.com>
2023-04-20 12:33:55 +0200

    apprenant connecter detail

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,17 @@ use App\Models\User;
 use App\Models\Presence;
 use App\Models\Apprenant;
 use Illuminate\Http\Request;
+use App\Models\PromoReferentiel;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use Laravel\Sanctum\PersonalAccessToken;
 use App\Http\Resources\ApprenantResource;
+use App\Models\PromoReferentielApprenant;
 use App\Http\Controllers\PresenceController;
 use App\Http\Controllers\ApprenantController;
+use App\Http\Resources\PromoReferentielResource;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +36,18 @@ Route::get('/apprenant', function (Request $request) {
     $user = explode(" ",$request->headers->all()["authorization"][0]);
     $token = PersonalAccessToken::findToken($user[1]);
     //  dd($token->tokenable->id);
-    return new ApprenantResource(Apprenant::find($token->tokenable->id));
+
+    $apprenant = Apprenant::find($token->tokenable->id);
+    $promoReferentielId=PromoReferentielApprenant::where(['apprenant_id'=> $apprenant->id])->first();
+    $promoReferentiel= PromoReferentiel::where(['id'=> $promoReferentielId->promo_referentiel_id])->first();
+
+
+    return[
+        "apprenant"=> new ApprenantResource($apprenant),
+        "promoReferentiel"=> new PromoReferentielResource($promoReferentiel)
+    ];
+
+
 
  });
 
