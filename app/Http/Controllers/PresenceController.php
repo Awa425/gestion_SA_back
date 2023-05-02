b968cef6736c0f616706724e987b0a3b7390b8b3
mouhamed3008 <mouha712@gmail.com>
2023-05-02 12:16:47 +0200

    presence

--- app/Http/Controllers/PresenceController.php
+++ app/Http/Controllers/PresenceController.php
@@ -17,20 +17,14 @@ class PresenceController extends Controller
 {
     public function index(Request $request)
     {
-        $date = $request->input('date');
-
-        if (!empty($date)) {
-            $presences = Presence::whereDate('date_heure_arriver', $date)->with('apprenants')->get();
-        } else {
-            $presences = Presence::whereDate('date_heure_arriver', Carbon::today())->with('apprenants')->get();
-        }
-
+        $date = $request->input('date') ? $request->input('date'): Carbon::today();
+        $presences = Presence::whereDate('date_heure_arriver', $date)->with('apprenants')->get();
         return new PresenceCollection($presences);
     }
 
-    
 
-    
+
+
 
     public function store(Request $request)
     {
@@ -40,28 +34,28 @@ class PresenceController extends Controller
         }
         $matricule = $request->matricule;
         $apprenant = Apprenant::where('matricule', $matricule)->first();
-    
+
         if (!$apprenant) {
             return response()->json(['error' => 'Apprenant not found'], 404);
         }
-    
+
         $presenceExiste = Presence::where('apprenant_id', $apprenant->id)
                                     ->whereDate('date_heure_arriver', $dateArrivee->toDateString())
                                     ->exists();
-    
+
         if ($presenceExiste) {
             return response()->json(['error' => 'Presence already exists'], 400);
         }
-    
+
         $presence = new Presence();
         $presence->date_heure_arriver = $dateArrivee;
-        $presence->apprenant_id = $apprenant->id ; 
+        $presence->apprenant_id = $apprenant->id ;
         $presence->save();
         $presence->apprenants()->attach($apprenant->id);
-    
+
         return new ApprenantResource($apprenant);
     }
-    
+
 
 
     public function show(Request $request, Presence $presence)
