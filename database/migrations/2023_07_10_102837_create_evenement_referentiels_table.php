1b09b69cdacf0ffe86d1f913880baa603b78584b
mouhamed3008 <mouha712@gmail.com>
2023-07-13 12:32:33 +0000

    promo_ref

---
+++ database/migrations/2023_07_10_102837_create_evenement_referentiels_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('evenement_referentiels', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreign('evenement_id')
+            ->references('id')
+            ->on('evenements') ;
+            $table->foreign('promo_referentiel_id')
+            ->references('id')
+            ->on('promo_referentiels') ;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('evenement_referentiels');
+    }
+};
