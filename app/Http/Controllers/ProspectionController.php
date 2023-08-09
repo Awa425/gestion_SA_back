6fc9c1ca531adc731bfa848ad550eafffe665725
mouhamed3008 <mouha712@gmail.com>
2023-08-09 09:47:55 +0000

    prospection

---
+++ app/Http/Controllers/ProspectionController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Prospection;
+use Illuminate\Http\Request;
+
+class ProspectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Prospection $prospection)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Prospection $prospection)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Prospection $prospection)
+    {
+        //
+    }
+}
